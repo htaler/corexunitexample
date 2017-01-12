d3e3a0dd36ef3bbbd009d7dc8f8bc901d9ec0946
Hubert Taler <hubert.taler@blstream.com>
2017-01-12 08:40:47 +0100

    First commit

---
+++ src/PrimeService/PrimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public void IsPrime(int candidate)
+        {    
+		throw new NotImplementedException("Please implement");
+        }
+    }
+}
---
+++ test/PrimeServices.Test/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using Prime.Services;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+
+	[Fact]
+	public void CanCreateInstance()
+	{
+		var service = new PrimeService();
+		Assert.NotNull(service);
+	}
+    }
+}
